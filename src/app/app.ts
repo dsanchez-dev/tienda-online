a00458191aa3e78a56a3cd2ab85357938d35230b
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2025-06-17 14:21:11 -0600

    P03 - Mostrar componentes en la página principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListadoProductos } from "./listado-productos/listado-productos";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListadoProductos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'tienda-online';
+  protected titulo = 'Tienda Online';
 }
